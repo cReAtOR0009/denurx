36ecb041142a2c1ae501ed53159150294efc7caf
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-05-27 17:52:51 +0100

    using gsap to animate nav item on mouse over and mouseleave, animating logo and butoon with gsap, increased button size for mobile, made conditonal rendering of sidebar from  working directly (using &&) to working from tailwind (using hiden and flex)

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,34 @@
 import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import { styles } from "../styles";
 import { nav } from "../assets/text";
 import { menu, close } from "../assets/images";
 import { animateNavItems, animateFromTo, animateButton, revealDivOnScroll } from "../assets/animation";
-import { useGSAP } from "@gsap/react";
 
 const NavItem = ({ title, active, handleClick, mobile, index }) => {
+  const handleMouseEnter = (event) => {
+    const navItem = event.currentTarget;
+    gsap.to(navItem, { y: 10, duration: 0.3 });
+  };
+
+  const handleMouseLeave = (event) => {
+    const navItem = event.currentTarget
+    gsap.to(navItem, { y: 0, duration: 0.3 });
+  };
+
   return (
     <li
       onClick={() => handleClick(title)}
+      onMouseEnter={handleMouseEnter} 
+      onMouseLeave={handleMouseLeave}
       className={`${
         active === title ? "border-b-4 text-secondary" : "text-white"
       } ${
         mobile ? "mb-6" : "mr-4 md:mr-8"
-      } inline-block mb-4 uppercase border-b-secondary py-2 px-4 cursor-pointer hover:border-b-4 hover:border-b-secondary_100 hover:text-secondary_100 hover:animate-fade-in-down`}
+      } nav-item inline-block mb-4 uppercase border-b-secondary py-2 px-4 cursor-pointer hover:border-b-4 hover:border-b-secondary_100 hover:text-secondary_100`}
     >
       <HashLink to={`/denurx/#${title.toLocaleLowerCase()}`}>{title}</HashLink>
     </li>
@@ -24,6 +37,9 @@ const NavItem = ({ title, active, handleClick, mobile, index }) => {
 
 const Nav = () => {
   const containerRef = useRef(null)
+  const aside = useRef(null)
+  const logoRef = useRef(null)
+  const buttonRef = useRef(null)
   const [active, setActive] = useState(nav[0].title);
   const [activeMenu, setActiveMenu] = useState(false);
   const navRef = useRef(null);
@@ -36,10 +52,24 @@ const Nav = () => {
     revealDivOnScroll(containerRef)
     const navItems = navRef.current.querySelectorAll("li");
     animateNavItems(navItems);
-    animateFromTo("#logo", "x", -150, 0);
-    animateFromTo("#action_button1", "x", -150, 0, 1);
+    animateFromTo(logoRef, "x", -150, 0);
+    animateFromTo(buttonRef, "x", -150, 0, 1);
     // animateButton('#action_button1');
   }, []);
+  
+  useGSAP(() => {
+    // const tl = gsap.timeline();
+    if (activeMenu) {
+      animateFromTo(aside, "x", 250, 0, 0, 1 )
+    } else {
+      animateFromTo(aside, "x", 0, 250, 0, .5 )
+    }
+    // animateFromTo(aside, "x", 250, 0, 0, .5 )
+    // return () => {
+    //   tl.kill(); // Clean up the timeline on component unmount
+    // };
+
+  }, [activeMenu])
 
   
 
@@ -52,6 +82,7 @@ const Nav = () => {
       {/* <a id="logo" href="#home" rel="noopener noreferrer" className=""> */}
       <HashLink to={"/denurx/#home"}>
         <img
+          ref={logoRef}
           src="./logo.png"
           alt="Logo here"
           className="w-12 h-12 md:w-16 md:h-16"
@@ -73,9 +104,9 @@ const Nav = () => {
         </ul>
       </div>
       {/* <a  id="action_button1" href="#join wait list" rel="noopener noreferrer"> */}
-      <Link to={"/denurx/waitlist"}>
+      <Link ref={buttonRef} to={"/denurx/waitlist"}>
         <button
-          className={`rounded-[30px] animate-pulse bg-primaryBtn px-4 py-2 sm:px-6 sm:py-4 text-xxs text-white sm:text-base font-medium uppercase leading-normal  text-primary-700 border border-[transparent] hover:bg-secondary_100 focus:border-white transition duration-150`}
+          className={`rounded-[30px] animate-pulse bg-primaryBtn px-6 py-4 sm:px-6 sm:py-4 text-xxs text-white sm:text-base font-medium uppercase leading-normal  text-primary-700 border border-[transparent] hover:bg-secondary_100 focus:border-white transition duration-150`}
         >
           Join Waitlist
         </button>
@@ -91,8 +122,8 @@ const Nav = () => {
         </div>
       </div>
 
-      {activeMenu && (
-        <div className="lg:hidden absolute top-0 w-[50%] h-screen right-0 p-4 navSidebar bg-white shadow-featuresCardShadow hover:shadow-featuresCardHover">
+      {/* {activeMenu && ( */}
+        <aside ref={aside} className={`${activeMenu?"flex":"hidden"} lg:hidden absolute top-0 w-[50%] h-screen right-0 p-4 navSidebar bg-white shadow-featuresCardShadow hover:shadow-featuresCardHover`}>
           <div
             onClick={() => setActiveMenu(false)}
             class="group flex lg:hidden h-10 w-10 cursor-pointer items-center justify-center rounded-3xl bg-whit p-2"
@@ -122,8 +153,8 @@ const Nav = () => {
               Join Waitlist
             </button>
           </Link>
-        </div>
-      )}
+        </aside>
+      {/* )} */}
     </nav>
   );
 };
