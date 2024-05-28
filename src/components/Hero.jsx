322c9de04ad0876a58918168c13a0a0ddda429d9
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-05-28 17:49:30 +0100

    added beating animation on hover and changed secondary button on hover

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,10 +16,10 @@ const Hero = () => {
         <h1 className={`${styles.h1}  py-4 md:py-2 lg:py-6 text-7xl sm:text-8xl md:text-8xl lg:text-6xl xl:text-8xl font-bold leading-tight text-white mt-20 `}>{HeroText.h1}</h1> 
         <p className=' text-base sm:text-base md:text-lg lg:text-[20px] xl:text-xl leading-relaxed text-white'>{HeroText.tagLine}</p> 
         <div className=' flex gap-6 sm:gap-8 sm:py-4 md:py-4 my-4'>
-        <HashLink to="/denurx/#about"><button className={` rounded-[30px] bg-primary px-6 py-4 sm:px-6 sm:py-4 text-xxs text-white sm:text-base font-medium uppercase leading-normal text-primary-700 border border-[transparent] hover:bg-primary_200 focus:border-white transition duration-150`}>Learn More</button></HashLink>
+        <HashLink to="/denurx/#about"><button className={` rounded-[30px] bg-primary px-6 py-4 sm:px-6 sm:py-4 text-xxs text-white sm:text-base font-medium uppercase leading-normal text-primary-700 border border-[transparent] hover:bg-[transparent] hover:border-white focus:border-white transition duration-150`}>Learn More</button></HashLink>
         <button
         onClick={() => setShowJoinwaitlist(true)}
-          className={`rounded-[30px] animate-pulse bg-primaryBtn outline-none  px-6 py-4 sm:px-6 sm:py-4 text-xxs text-white sm:text-base font-medium uppercase leading-normal  text-primary-700 border border-[transparent] hover:animate-shine focus:border-white transition duration-150`}
+          className={`rounded-[30px] animate-pulse bg-primaryBtn outline-none  px-6 py-4 sm:px-6 sm:py-4 text-xxs text-white sm:text-base font-medium uppercase leading-normal  text-primary-700 border border-[transparent] hover:animate-pop-up-infinite focus:border-white transition duration-150`}
         >
           Join Waitlist
         </button>
