0f64ff6efd686d39465309863f2023f204526714
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-05-28 11:14:16 +0100

    removed unused style animating menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -66,7 +66,7 @@ const Nav = () => {
       </div>
         <button
         onClick={() => setShowJoinwaitlist(true)}
-          className={`rounded-[30px] animate-pulse bg-primaryBtn px-6 py-4 sm:px-6 sm:py-4 text-xxs text-white sm:text-base font-medium uppercase leading-normal  text-primary-700 border border-[transparent] hover:bg-secondary_100 focus:border-white transition duration-150`}
+          className={`rounded-[30px] animate-pulse bg-primaryBtn px-6 py-4 sm:px-6 sm:py-4 text-xxs text-white sm:text-base font-medium uppercase leading-normal  text-primary-700  hover:bg-secondary_100 focus:border-white transition duration-150`}
         >
           Join Waitlist
         </button>
@@ -85,7 +85,7 @@ const Nav = () => {
       <aside
         className={`${
           activeMenu ? "flex" : "hidden"
-        } flex-col lg:hidden absolute top-0 w-[50%] h-screen right-0 p-4 bg-[#cacaca] `}
+        } flex-col lg:hidden absolute top-0 w-[50%] h-screen right-0 p-4 bg-[#cacaca] animate-slide-in-right`}
       >
         <div
           onClick={() => setActiveMenu(false)}
