cf863b8d5e479c8e5476bbfdea338c3ce6c48e35
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-06-07 11:25:12 +0100

    modifies services to link to another page

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,35 +1,65 @@
-import  { useState } from 'react'
-import { services } from '../assets/text'
-import { styles } from '../styles'
+import { useState } from "react";
+import { HashLink } from "react-router-hash-link";
+import { services } from "../assets/text";
+import { arrow } from "../assets/images";
+import { styles } from "../styles";
 
-const ServiceCard = ({icon, title, description, image}) => {
-    const trimmedContent = `${description.slice(0, 200)}...`
-    const [readMore, setReadMore] = useState(false)
+export const ServiceCard = ({ icon, title, description, image }) => {
+  const trimmedContent = `${description.slice(0, 200)}...`;
+  const [readMore, setReadMore] = useState(false);
 
-    return (
-        <div key={title} className='relative rounded-md sm:rounded-xl border-2 border-[#cacaca]  cursor-pointer hover:transition-colors transition:border ' onClick={() => setReadMore(!readMore)}>
-            <img src={image} alt=""  className='w-[100%] rounded-md sm:rounded-xl '/>
-            <div className='p-4'>
-                <img src={icon} alt={`${title}`} className='mt-[-12%] w-8 lg:w-20 md:w-20 ' />
-                <h3 className={`${styles.h3} text-primary`}>{title}</h3>
-                <p className={`${styles.paragraph} text-black`}>{readMore?description:trimmedContent}<button onClick={() => setReadMore(!readMore)} className={`${readMore?"bg-secondary_100 group-hover:bg-primary":"bg-primary "} text-white inline-block text-xxs md:text-sm  rounded-full cursor-pointer p-2`}>{readMore?"Read less":"Read more"}</button></p>
-            </div>  
-        </div>
-    )
-}
-
-const Services = () => {
-  
   return (
-    <section  id='services' className={`${styles.container} flex flex-col my-4 gap-6 sm:gap-10`} >
-        <h2 className={`${styles.h2}`}>Our High Quality Service</h2>
-        <div  className='service_grid py-4 sm:py-6'>
-            {services.map((service, index) => (
-                <ServiceCard key={index} {...service} />
-            ))}
+    <HashLink to={`/denurx/services/#${title}`}>
+      <div
+        key={title}
+        id="title"
+        className="serciveCard relative rounded-[20px] sm:rounded-[50px] border-1 border-[#cacaca]  cursor-pointer hover:transition-colors transition:border max-w-[500px] hover:scale-[1.1] transition"
+        onClick={() => setReadMore(!readMore)}
+      >
+        <img
+          src={image}
+          alt=""
+          className="w-[100%] rounded-[20px] sm:rounded-[50px] "
+        />
+        <img
+          src={arrow}
+          alt=""
+          className="absolute top-0 right-0 z-10 w-[40px] sm:w-[80px] lg:w-[80px] xl:w-[80px] cursor-pointer rounded-full hover:scale-[1.1] transition"
+        />
+        <div className="p-4 absolute bottom-0 left-0 right-0 rounded-[20px] sm:rounded-[50px] bg-black bg-opacity-30">
+          <img
+            src={icon}
+            alt={`${title}`}
+            className="mt-[-12%] w-10 lg:w-20 md:w-20"
+          />
+          <h3 className={`${styles.h3} text-white`}>{title}</h3>
+          {/* <p className={`${styles.paragraph} text-black`}>{readMore?description:trimmedContent}
+                    <button onClick={() => setReadMore(!readMore)} className={`${readMore?"bg-secondary_100 group-hover:bg-primary":"bg-primary "} text-white inline-block text-xxs md:text-sm  rounded-full cursor-pointer p-2`}>{readMore?"Read less":"Read more"}</button>
+                    </p> */}
         </div>
-    </section>
-  )
-}
+      </div>
+    </HashLink>
+  );
+};
 
-export default Services
+const Services = ({ services }) => {
+  return (
+    <section id="services" className={`${styles.container} relative`}>
+      <div className="services flex flex-col my-4 gap-6 sm:gap-10">
+        <div className="flex items-center justify-between flex-wrap">
+          <h2 className={`${styles.h2}`}>Our High Quality Service</h2>
+          <HashLink to={`/denurx/services`} className="underline decoration-primary hover:scale-[1.1] transition">
+            <h4 className={`${styles.h3}`}>All Services</h4>
+          </HashLink>
+        </div>
+        <div className="flex  gap-4 sm:gap-6 flex-wrap xl:flex-nowrap py-4 sm:py-6">
+          {services.map((service, index) => (
+            <ServiceCard key={index} {...service} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Services;
