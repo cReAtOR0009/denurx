f6537a57085d8942cd0a5fc324b9e51218a0b6ae
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-06-09 19:57:21 +0100

    multistep setting state for response

--- src/components/form/MultistepForm.jsx
+++ src/components/form/MultistepForm.jsx
@@ -1,6 +1,4 @@
-// src/components/MultiStepForm.js
 import React, { useState } from "react";
-import { styles } from "../../styles";
 
 const MultiStepForm = () => {
   const [step, setStep] = useState(1);
@@ -21,6 +19,16 @@ const MultiStepForm = () => {
     setStep(step - 1);
   };
 
+  const handleProviderSubmit = (data) => {
+    console.log("Provider data:", data);
+    // Handle provider registration (e.g., API call)
+  };
+
+  const handleUserSubmit = (data) => {
+    console.log("User data:", data);
+    // Handle user registration (e.g., API call)
+  };
+
   return (
     <div
       className={`${
@@ -32,10 +40,10 @@ const MultiStepForm = () => {
           <StepOne onProviderSelection={handleProviderSelection} />
         )}
         {step === 2 && isProvider === true && (
-          <ProviderRegistrationForm onBack={handleBack} />
+          <ProviderRegistrationForm onBack={handleBack} onSubmit={handleProviderSubmit} />
         )}
         {step === 2 && isProvider === false && (
-          <UserRegistrationForm onBack={handleBack} />
+          <UserRegistrationForm onBack={handleBack} onSubmit={handleUserSubmit} />
         )}
       </div>
 
@@ -69,88 +77,121 @@ const StepOne = ({ onProviderSelection }) => (
   </div>
 );
 
-const ProviderRegistrationForm = ({ onBack }) => (
-  <div className={`animate-slide-in text-white`}>
-    <h2 className="text-2xl font-bold mb-4">Provider Registration</h2>
-    <form>
-      {/* Add your form fields here */}
-      <div className="mb-4 ">
-        <label className="block text-gray-700">Name</label>
-        <input type="text" className="w-full p-2 border rounded text-black" />
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-700 ">Professional Title:</label>
-        <select name="professionalTitle" id="professionalTitle" className="w-full p-2 border rounded text-black">
-          <option value="doctor">Doctor</option>
-          <option value="nurse">Nurse</option>
-          <option value="therapist">Therapist</option>
-          <option value="pharmacist">Pharmacist</option>
-          <option value="dentist">Dentist</option>
-          <option value="optometrist">Optometrist</option>
-          <option value="physicianAssistant">Physician Assistant</option>
-          <option value="chiropractor">Chiropractor</option>
-          <option value="other">Other</option>
-        </select>
+const ProviderRegistrationForm = ({ onBack, onSubmit }) => {
+  const [providerData, setProviderData] = useState({
+    name: "",
+    professionalTitle: "",
+    affiliations: "",
+    email: ""
+  });
 
-        {/* <input type="email" className="w-full p-2 border rounded" /> Professional Affiliations: */}
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">Professional Affiliations</label>
-        <input type="text" className="w-full p-2 border rounded text-black" />
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">Email</label>
-        <input type="email" className="w-full p-2 border rounded text-black" />
-      </div>
-      <div className="flex justify-center items-center gap-6">
-        <button
-          type="button"
-          className={`inline-block md:text-sm rounded-full cursor-pointer p-2 bg-secondary_100 outline-none  px-4 py-2 sm:px-4 sm:py-2 text-xxs text-white sm:text-base font-medium border focus:border-white`}
-          onClick={onBack}
-        >
-          Back
-        </button>
-        <button
-          type="submit"
-          className="inline-block md:text-sm rounded-full cursor-pointer p-2 bg-primary outline-none  px-4 py-2 sm:px-4 sm:py-2 text-xxs text-white sm:text-base font-medium border focus:border-white"
-        >
-          Register
-        </button>
-      </div>
-    </form>
-  </div>
-);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setProviderData({ ...providerData, [name]: value });
+  };
 
-const UserRegistrationForm = ({ onBack }) => (
-  <div className={`animate-slide-in text-white`}>
-    <h2 className="text-2xl font-bold mb-4">User Registration</h2>
-    <form>
-      {/* Add your form fields here */}
-      <div className="mb-4">
-        <label className="block text-gray-700">Name</label>
-        <input type="text" className="w-full p-2 border rounded text-black" />
-      </div>
-      <div className="mb-4">
-        <label className="block text-gray-700">Email</label>
-        <input type="email" className="w-full p-2 border rounded text-black" />
-      </div>
-      <div className="flex justify-center items-center gap-6">
-        <button
-          type="button"
-          className={`inline-block md:text-sm rounded-full cursor-pointer p-2 bg-secondary_100 outline-none  px-4 py-2 sm:px-4 sm:py-2 text-xxs text-white sm:text-base font-medium border focus:border-white`}
-          onClick={onBack}
-        >
-          Back
-        </button>
-        <button
-          type="submit"
-          className="inline-block md:text-sm rounded-full cursor-pointer p-2 bg-primary outline-none  px-4 py-2 sm:px-4 sm:py-2 text-xxs text-white sm:text-base font-medium border focus:border-white"
-        >
-          Register
-        </button>
-      </div>
-    </form>
-  </div>
-);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(providerData);
+  };
+
+  return (
+    <div className={`animate-slide-in text-white`}>
+      <h2 className="text-2xl font-bold mb-4">Provider Registration</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700">Name</label>
+          <input type="text" name="name" value={providerData.name} onChange={handleChange} className="w-full p-2 border rounded text-black" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Professional Title</label>
+          <select name="professionalTitle" value={providerData.professionalTitle} onChange={handleChange} className="w-full p-2 border rounded text-black">
+            <option value="doctor">Doctor</option>
+            <option value="nurse">Nurse</option>
+            <option value="therapist">Therapist</option>
+            <option value="pharmacist">Pharmacist</option>
+            <option value="dentist">Dentist</option>
+            <option value="optometrist">Optometrist</option>
+            <option value="physicianAssistant">Physician Assistant</option>
+            <option value="chiropractor">Chiropractor</option>
+            <option value="other">Other</option>
+          </select>
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Professional Affiliations</label>
+          <input type="text" name="affiliations" value={providerData.affiliations} onChange={handleChange} className="w-full p-2 border rounded text-black" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Email</label>
+          <input type="email" name="email" value={providerData.email} onChange={handleChange} className="w-full p-2 border rounded text-black" />
+        </div>
+        <div className="flex justify-center items-center gap-6">
+          <button
+            type="button"
+            className={`inline-block md:text-sm rounded-full cursor-pointer p-2 bg-secondary_100 outline-none  px-4 py-2 sm:px-4 sm:py-2 text-xxs text-white sm:text-base font-medium border focus:border-white`}
+            onClick={onBack}
+          >
+            Back
+          </button>
+          <button
+            type="submit"
+            className="inline-block md:text-sm rounded-full cursor-pointer p-2 bg-primary outline-none  px-4 py-2 sm:px-4 sm:py-2 text-xxs text-white sm:text-base font-medium border focus:border-white"
+          >
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+const UserRegistrationForm = ({ onBack, onSubmit }) => {
+  const [userData, setUserData] = useState({
+    name: "",
+    email: ""
+  });
+  
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(userData);
+  };
+
+  return (
+    <div className={`animate-slide-in text-white`}>
+      <h2 className="text-2xl font-bold mb-4">User Registration</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700">Name</label>
+          <input type="text" name="name" value={userData.name} onChange={handleChange} className="w-full p-2 border rounded text-black" />
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700">Email</label>
+          <input type="email" name="email" value={userData.email} onChange={handleChange} className="w-full p-2 border rounded text-black" />
+        </div>
+        <div className="flex justify-center items-center gap-6">
+          <button
+            type="button"
+            className={`inline-block md:text-sm rounded-full cursor-pointer p-2 bg-secondary_100 outline-none  px-4 py-2 sm:px-4 sm:py-2 text-xxs text-white sm:text-base font-medium border focus:border-white`}
+            onClick={onBack}
+          >
+            Back
+          </button>
+          <button
+            type="submit"
+            className="inline-block md:text-sm rounded-full cursor-pointer p-2 bg-primary outline-none  px-4 py-2 sm:px-4 sm:py-2 text-xxs text-white sm:text-base font-medium border focus:border-white"
+          >
+            Register
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default MultiStepForm;
