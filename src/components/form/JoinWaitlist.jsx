3eb72c0df97251e8b6dd77737f1f78e47bee8809
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-06-15 04:03:14 +0100

    using multistepform for wailtlist

--- src/components/form/JoinWaitlist.jsx
+++ src/components/form/JoinWaitlist.jsx
@@ -1,15 +1,16 @@
 import { styles } from "../../styles";
 import MailChimp from "./MailChimp";
+import MultiStepForm from "./MultistepForm";
 
 const JoinWaitlist = ({ setShowJoinwaitlist }) => {
   return (
     <section
       id="join wait list"
-      class={` h-screen px-4 py-6 sm:p-6  md:p-6 lg:p-20 xl:p-6  absolute z-50 top-0 left-0 bottom- right-0 flex flex-col justify-center items-center my-0 mx-[auto] w-[100%] bg-[#cacacaf0] animate-pop-up`}
+      class={` h-screen px-4 py-6 sm:p-6  md:p-6 lg:p-20 xl:p-6 pt-[20%] sm:pt-[20%] md:pt-[20%] xl:pt-[0%]  absolute z-50 top-0 left-0 bottom-0 right-0 flex flex-col justify-center items-center my-0 mx-[auto]  w-[100%]  bg-[#cacacaf0] animate-pop-up overflow-y-auto`}
     >
       <div
         onClick={() => setShowJoinwaitlist(false)}
-        class="absolute z-50 right-0 top-2 p-2  mr-4 space-y-2 group group-hover:scale-[1.1] select-none cursor-pointer"
+        class="fixed z-50 right-0 top-2 p-2  mr-4 space-y-2 group group-hover:scale-[1.1] select-none cursor-pointer"
       >
         <span class="block h-1 w-6 sm:h-1 sm:w-10 origin-center rounded-full bg-black transition-transform ease-in-out rotate-45 translate-y-1.5 group-hover:bg-primary_100"></span>
         <span class="block h-1 w-6 sm:h-1 sm:w-10  origin-center rounded-full bg-black transition-transform ease-in-out -rotate-45  -translate-y-1.5 group-hover:bg-primary_100"></span>
@@ -32,7 +33,8 @@ const JoinWaitlist = ({ setShowJoinwaitlist }) => {
         recommendations.
         Sign up for our waitlist and be the first to know when we launch!
       </p>
-      <MailChimp />
+      {/* <MailChimp /> */}
+      <MultiStepForm />
     </section>
   );
 };
