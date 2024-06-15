bfdf3ee18ed231a4de5d6275f516e91c8be9907d
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-06-15 04:04:28 +0100

    added styling for new changesn and component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import Hero from "../components/Hero";
 import AboutApp from "../components/AboutApp";
-import HowItWorks from "../components/HowItWorks";
+import HowItWorks from "../components/Steps";
 import Features from "../components/Features";
 import Services from "../components/Services";
 import Testimonials from "../components/Testimonials";
 import Faq from "../components/Faq";
 import { services } from "../assets/text";
 import MultiStepForm from "../components/form/MultistepForm";
+import Blog from "../components/Blog";
+import Registration from "../components/Registration";
 
 const Home = () => {
   return (
@@ -14,11 +16,13 @@ const Home = () => {
       <div className="bg-heroGradient fullbg">
         <Hero />
       </div>
+      <Registration />
       <AboutApp />
-        <MultiStepForm />
+        {/* <MultiStepForm /> */}
       <HowItWorks />
       <Features />
       <Services services={services} />
+      <Blog />
       <Testimonials />
       <Faq />
     </div>
