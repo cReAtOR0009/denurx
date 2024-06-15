805945c38efa893f04fef26d77a89efafe98cde0
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-06-15 03:57:11 +0100

    added blog section

---
+++ src/components/Blog.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { styles } from "../styles";
+import { blogs } from "../assets/text";
+
+const Blog = () => {
+  const BlogCard = ({ title, content, summary, image, date, author }) => {
+    return (
+      <div className="bg-white rounded-lg shadow-md overflow-hidden">
+        <img src={image} alt={title} className="w-full h-48 object-cover" />
+        <div className="p-2 sm:p-6">
+          <h3 className={`${styles.h3} mb-2`}>{title}</h3>
+          <p className={`${styles.paragraph} text-[#939191] mb-4`}>
+            {date} - {author}
+          </p>
+          <p className={`${styles.paragraph}  mb-4`}>{summary}</p>
+          <Link to="#" className="text-primary-500 hover:underline">
+            Read more
+          </Link>
+        </div>
+      </div>
+    );
+  };
+  return (
+    <div className={`${styles.container} relative mx-auto px-4`}>
+      <h2 className={`${styles.h2}`}>Our Blog</h2>
+      <div className={`blog grid gap-8 lg:grid-cols-2 xl:grid-cols-3`}>
+        {blogs.map((blog, index) => (
+          <BlogCard {...blog} key={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
