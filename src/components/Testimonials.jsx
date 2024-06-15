777efcf216e49d8346dacec71c30d87319e3ba8b
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-06-15 04:02:50 +0100

    added before column to testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -5,7 +5,7 @@ import { styles } from "../styles";
 const TestimonialCard = ({ image, name, role, testament, gridPosition }) => {
 
   return (
-    <li className={`${gridPosition} relative flex flex-col gap-4 text-sm leading-6 p-4 bg-primary_20  rounded-lg text-black border-2 border-[#cacaca] hover:bg-primary hover:text-white`}>
+    <li className={`${gridPosition} testimonialsContainer relative flex flex-col gap-4 text-sm leading-6 p-4 bg-primary_20  rounded-lg text-black border-2 border-[#cacaca] hover:bg-primary hover:text-white`}>
       <div className="fle items-center gap-2 md:gap-4">
         {/* <img
           src={image}
@@ -25,8 +25,8 @@ const TestimonialCard = ({ image, name, role, testament, gridPosition }) => {
 const Testimonials = () => {
 
   return (
-    <section  id="testimonials" className={`${styles.container} testimonials relative border-[#eaeaea] bg-[#eaecf0a7]`}>
-      <div className={` relative`}>
+    <section  id="testimonials" className={`${styles.container}  relative border-[#eaeaea] bg-[#eaecf0a7]`}>
+      <div className={` testimonials `}>
         <div className="transition duration-500 ease-in-out transform scale-100 translate-x-0 translate-y-0 opacity-100">
           <div className="mb-6  md:mb-10">
             <h2 className={`${styles.h2} `}>
@@ -35,8 +35,8 @@ const Testimonials = () => {
            
           </div>
         </div>
-        <div className=""> 
-          <ul className="testimonial_card flex flex-wrap lg:flex-nowrap gap-4 my-4 overflow--scroll">
+        <div className="relative "> 
+          <ul className="testimonial_card  flex flex-wrap lg:flex-nowrap gap-4 my-4 overflow--scroll">
             {testimonials.map((testimonial, index) => {
             
               return (
