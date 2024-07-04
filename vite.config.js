37425a7234715085239a6904319d804128c84299
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-07-04 11:26:45 +0100

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// import { baseUrl } from './config';
 
-console.log("env base url:", process.env.VITE_BASE_URL)
+// console.log("env base url:", process.env.VITE_BASE_URL)
 export default defineConfig({
   plugins: [react()],
-  base: "/denurx",
+  base: process.env.VITE_BASE_URL?"":"/denurx",
   build: {
     target: 'esnext', // Set the target to esnext for modern JavaScript support
     outDir: 'dist',   // Output directory
