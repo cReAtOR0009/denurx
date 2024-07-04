fbc4010f6d0dbf432b8e562a91f332fdf8013fda
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-07-04 11:42:32 +0100

    modified vite config

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // Assuming you're using Vite, and the environment variable is VITE_APP_MODE
 
-const baseUrl = import.meta.env.VITE_APP_MODE === "DEV" ? "/denurx" : "";
+const VITE_APP_MODE = "DEV"
+const baseUrl = VITE_APP_MODE === "DEV" ? "/denurx" : "";
 
 
 export { baseUrl };
