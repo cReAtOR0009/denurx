1dc66365140ecf1423cbe3e73ade2d453cebabd1
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-07-04 11:24:43 +0100

    udated config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+// import { baseUrl } from './config';
 
-// console.log("env base url:", process.env.VITE_BASE_URL)
+console.log("env base url:", process.env.VITE_BASE_URL)
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_URL,
+  base: "/denurx",
   build: {
     target: 'esnext', // Set the target to esnext for modern JavaScript support
     outDir: 'dist',   // Output directory
