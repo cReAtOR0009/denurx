eadcea0f483a708c3e5db29a303dfed66df28c63
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-07-08 16:16:53 +0100

    fix distorted overlay for mobile, using seperated h4 style, grid-col-1 display for mobile, diaplay flex-row for large screen, reduced image size, reduced border radius for overlay

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -30,7 +30,7 @@ const StepsCard = ({
     <div
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
-      className={`relative flex-1 flex flex-col gap-2 justify-between items-center border-2 border-[#87898c] rounded-2xl p-2 sm:p-2 lg:p-4 h-[auto] min-w-[150px] bg-[#eaecf0a7] dark:bg-dark-background3 text-left sm:hover:scale-[1.1] transition`}
+      className={` flex-1 flex flex-col gap-2 justify-between items-center border-2 border-[#87898c] rounded-2xl p-2 sm:p-2 lg:p-4 h-[auto] min-w-[150px bg-[#eaecf0a7] dark:bg-dark-background3 text-left sm:hover:scale-[1.1] transition`}
       >
        {/* <div className="absolute top-0 right-0 transform translate-x-1/2 -translate-y-1/2 animate-bounce">
         <svg
@@ -48,7 +48,7 @@ const StepsCard = ({
       </div> */}
       
       <img src={icon} alt="" className="w-[40px] lg:w-[50px] h-[auto]" />
-      <h4 className={`${styles.h4} leading-[0]`}>{step}</h4>
+      <h4 className={`"${styles.h4}" text-base md:text-lg lg:text-sm xl:text-xl font-medium dark:text-dark-text leading-[0]`}>{step}</h4>
       {displayContent === description && (
         <div className="absolute flex sm:hidden flex-col items-center justify-center gap-2 p-4 h-[100%] rounded-2xl w-[100%] inset-0 bg-black bg-opacity-80 animate-slide-in-up text-left">
           <span
@@ -71,9 +71,9 @@ const Steps = () => {
   const [displayTipContent, setDisplayTipContent] = useState(null);
 
   return (
-    <section className={`${""} steps`}  >
-      <div className="flex flex-wrap md:flex-nowrap items-center  " title={"hover on each steps to get additional"}>
-        <div className=" relative flex- grid  grid-cols-2 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 w-[100%] md:w-[50%] gap-2 sm:gap-6 p-0 sm:p-2 mt-4 sm:mt-0 ">
+    <section className={`${""} steps`}>
+      <div className="flex flex-wrap flex-col lg:flex-row md:flex-nowrap items-center gap-4  " title={"hover on each steps to get additional"}>
+        <div className=" relative flex-1 grid  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 w-[100%] lg:w-[50%] gap-2 sm:gap-6 p-0 sm:p-2 mt-4 sm:mt-0 ">
           {howItWorks.map((step, index) => (
             <StepsCard
               key={index}
@@ -86,11 +86,11 @@ const Steps = () => {
             />
           ))}
         </div>
-        <div className="order- sm:order-[-1] lg:order-2 hidden sm:block relative flex- rounded-2xl sm:rounded-[50px] magicpattern2 bg-100 w-[100%] md:w-[50%]">
+        <div className="order- sm:order-[-1] lg:order-2 hidden sm:block relative flex- rounded-2xl lg:rounded-[50px] magicpattern2 bg-100 w-[100%] lg:w-[50%]">
           <img
             src={doctor5}
             alt=""
-            className="w-[100%] h-auto"
+            className="w-[100%] h-[400px]"
           />
           {/* <img
             src={doctor5}
@@ -98,7 +98,7 @@ const Steps = () => {
             className="block md:hidden mt-[-20px] sm:mt-[-30px] md:mt-[-50px] w-[100%] h-auto"
           /> */}
           {displayDescription && (
-            <div className="absolute hidden sm:flex flex-col justify-center gap-2 p-4 h-[100%] w-[100%] inset-0 rounded-2xl sm:rounded-[50px] bg-black bg-opacity-80 animate-slide-in-up text-left">
+            <div className="absolute hidden sm:flex flex-col justify-center gap-2 p-4 h-[100%] w-[100%] inset-0 rounded-2xl lg:rounded-[50px] bg-black bg-opacity-80 animate-slide-in-up text-left">
               <p className={`text-white ${styles.tip} text-left`}>
                 {displayContent}
               </p>
