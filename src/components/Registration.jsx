22019068633997bf93db6a1e27b3e93c3267c7da
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-07-08 16:32:52 +0100

    added margin top to article in mobile, text justify for small screen and above

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -13,7 +13,7 @@ const Registration = () => {
   return (
     <div className={`${""} register  dark:bg-dark-bg`}>
       <div className=" flex justify-center  md:justify-between  items-center flex-wrap sm:flex-nowrap ">
-        <div  className=" order-2 sm:order-1 flex  flex-col gap-2 sm:w-1/2 text-left sm:px-4 py-0 lg:px-10">
+        <div  className=" order-2 sm:order-1 flex  flex-col gap-2 sm:w-1/2 text-left mt-2 sm:mt-0 sm:px-4 py-0 lg:px-10">
           <h3 className={`${styles.h2} `}>{aboutApp.titleProviders}</h3>
           {/* <article className={`${styles.paragraph} text-primary`}>
             {aboutApp.textProviders}
@@ -21,7 +21,7 @@ const Registration = () => {
           {/* <p className={`${styles.paragraph}`}>
             Join Our Healthcare Providers Waiting list below
           </p> */}
-          <article className={`${styles.paragraph} text-justify`}>
+          <article className={`${styles.paragraph} sm:text-justify`}>
           Delivering virtual care as a Doctor is a game-changer. You can leverage out platform, and conduct secure video consultations, remote monitoring, and more. This approach enhances patient access and engagement, and helps you to expand reach and patient base. Virtual care also improves health outcomes and patient satisfaction. By adopting virtual care you can stay ahead in this digital age, grow your patient base, and deliver high-quality care conveniently and securely. Embrace this practice and improve patient care.
 
           </article>
@@ -82,7 +82,7 @@ const Registration = () => {
             className=" sm:h-[300px] md:h-[400px]  lg:h-[500px] w-[full] md:w-[auto]"
           />
         </div>
-        <div className="order-1 sm:order-2 flex flex-col gap-2 sm:w-1/2 text-left sm:px-4 py-0  lg:px-10">
+        <div className="order-1 sm:order-2 flex flex-col gap-2 sm:w-1/2 text-left mt-2 sm:mt-0 sm:px-4 py-0  lg:px-10">
           <h3 className={`${styles.h2} `}>{aboutApp.titlePatients}</h3>
           {/* <article className={`${styles.paragraph}`}>
             {aboutApp.textPatients}
@@ -90,7 +90,7 @@ const Registration = () => {
           {/* <p className={`${styles.paragraph}`}>
             Join Our Patient Waiting list below
           </p> */}
-          <article className={`${styles.paragraph} text-justify`}>
+          <article className={`${styles.paragraph} sm:text-justify`}>
           With Denurx, you can access quality healthcare anytime, anywhere. No more waiting in line or traveling to a clinic. Whether you're at home, in the office, or on-the-go, you can connect with a healthcare professional and receive the care you need. We offer flexibility, convenience, and accessibility, making it easier than ever to prioritize your health and wellbeing. With our virtual care, you can receive schedule virtual appointments, receive medical advice, diagnosis, treatment, and support from the comfort of your own space via your smartphone. Get connected and take control of your health today!
 
           </article>
