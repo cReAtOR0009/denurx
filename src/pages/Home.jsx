09fd7a2ba3f63300e17b2940aaf254b756673bdb
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-08-03 08:55:12 +0100

    indentation, removed unwanted code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,36 +3,38 @@ import Hero from "../components/Hero";
 import KeyFeatures from "../components/KeyFeatures";
 import Steps from "../components/Steps";
 import About from "../components/About";
-import Services from "../components/Services";
-import Testimonials from "../components/Testimonials";
 import Faq from "../components/Faq";
+import Registration from "../components/Registration";
+import Form from "../components/Form";
+
+import Services from "../components/Services";
 import { services } from "../assets/text";
+import Testimonials from "../components/Testimonials";
 import MultiStepForm from "../components/form/MultistepForm";
 import Blog from "../components/Blog";
-import Registration from "../components/Registration";
 import { ProviderRegistrationForm } from "../components/form/ProviderForm";
 import { UserRegistrationForm } from "../components/form/UserRegistrationForm";
-import Form from "../components/Form";
 
 const Home = () => {
   return (
     <div>
-       <Helmet>
-       <title>Denurx - Your Clinic In Your Hands</title>
-        <meta name="description" content="Denurx - Your Clinic In Your Hands. Connect with doctors, book appointments, manage medications, and access home health services." />
+      <Helmet>
+        <title>Denurx - Your Clinic In Your Hands</title>
+        <meta
+          name="description"
+          content="Denurx - Your Clinic In Your Hands. Connect with doctors, book appointments, manage medications, and access home health services."
+        />
       </Helmet>
-      {/* <div className="bg-heroGradient fullbg "> */}
-        <Hero />
-      {/* </div> */}
+      <Hero />
       <Registration />
       <KeyFeatures />
-        {/* <MultiStepForm /> */}
+      {/* <MultiStepForm /> */}
       <Steps />
       {/* <Testimonials /> */}
       <About />
       {/* <Services services={services} /> */}
       {/* <Blog /> */}
-     <Form />
+      <Form />
       <Faq />
     </div>
   );
