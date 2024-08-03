c19f89639f0e648263de6d2c3d1f6b6c96f59c27
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-08-03 10:07:37 +0100

    styled footer for better responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,8 +17,8 @@ const Footer = () => {
     setShowPrivacy(true)
   }
   return (
-    <footer  id="footer" className={`${styles.footer_container} ${styles.paragraph} px-4 py-4 sm:px-10 md:py-6  lg:p-6 xl:px-20 xl:py-10  max-w-[100%] text-black border-t-2 border-[#0048ff44] bg-[#eaecf0a7] dark:bg-dark-backgroundNav font-bold flex flex-wrap justify-between items-center`}>
-      <div className="  flex justify-between items-center gap-6 w-screen md:w-[auto]" >
+    <footer  id="footer" className={`${styles.footer_container} ${styles.paragraph}  max-w-[100%] text-black border-t-2 border-[#0048ff44] bg-[#eaecf0a7] dark:bg-dark-backgroundNav font-bold flex flex-col xl:flex-row  flex-wrap justify-between items-stretch`}>
+      <div className="flex justify-between items-center gap-6  md:w-[auto]" >
         <div id="logo_container">
         <img src="/logo.png" alt="Logo here" className="w-12 h-12 md:w-28 md:h-28" />
       </div>
@@ -29,18 +29,20 @@ const Footer = () => {
           Join Waitlist
         </button>
       </div>
-        <div className="flex flex-col w-screen md:w-[auto] my-2">
-          <div className="flex"></div>
-          <div className="flex justify-between  sm:flex-row gap-4 sm:gap-6">
-            <a href="tel:+234 00000000" className="flex flex-col items-center p-2 sm:p-4 flex-1 text-center hover:scale-[1.2] transition-all">
+        <div className="flex flex-col  md:w-[auto] my-2">
+          <div className="flex xl:flex-row flex-wrap justify-center lg:justify-between items-center">
+
+          <div className="flex flex-wrap justify-between  sm:flex-row gap-4 sm:gap-6">
+            <a href="tel:+234 8076091961" className="flex flex-col items-center p-2 sm:p-4 flex-1 text-center hover:scale-[1.2] transition-all">
               <img src={telephone} alt="" className="w-6 md:w-12 rounded-full"  />  +234 8076091961
             </a>
-            <a  href="mailto:creator@gmail.com" className="flex flex-col items-center p-2 sm:p-4 flex-1 text-center hover:scale-[1.2] transition-all">
+            <a  href="mailto:denurx@gmail.com" className="flex flex-col items-center p-2 sm:p-4 flex-1 text-center hover:scale-[1.2] transition-all">
               <img src={mail} alt="" className="w-6 md:w-12 rounded-full" />denurx@gmail.com
             </a>
           </div>
-          <div className="flex flex-col items-center">
-            {/* <h5 className={`${styles.h5} my-2`}>Join us On Our Social Handles!</h5> */}
+          
+          <div className="flex flex-col items-center ">
+            <h5 className={`${styles.h5}`}>Join us On Our Social Handles!</h5>
           <div className="flex justify-between flex-wrap gap-2 sm:gap-6 my-2">
             {socials.map((social, index) => (
               <a href={`${social.link}`} target="_blank" key={index} className="flex flex-1 flex-col items-center  cursor-pointer hover:scale-[1.2] transition-all">
@@ -50,12 +52,14 @@ const Footer = () => {
             ))}
           </div>
           </div>
+          </div>
+          <div className="w-full h-[2px] bg-[#0048ff44] rounded my-1"></div>
           <div>
             <div className="flex justify-between">
             <p onClick={() =>setShowTerms(true)} className={`hover:underline transition`}>Terms and Conditions &nbsp;</p> 
             <p onClick={() => setShowPrivacy(true)} className={`hover:underline transition`}> &nbsp; Privacy Policy</p>
             </div>
-            Copyright &copy; 2024 Denurx
+            <span className="float-right">Copyright &copy; 2024 Denurx</span> 
           </div>
         </div>
     { showPrivacy && <Privacy showPrivacy={showPrivacy} setShowPrivacy={setShowPrivacy}/>}
