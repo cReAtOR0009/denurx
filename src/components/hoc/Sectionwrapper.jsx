12e444d13033750cd05030cf1ea0e5001c5c2d2a
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-09-15 02:17:30 +0100

    added container prop to wrapper

--- src/components/hoc/Sectionwrapper.jsx
+++ src/components/hoc/Sectionwrapper.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { styles } from '../../styles';
 
-const Sectionwrapper = (Component, idName, backgroundProp) =>
+const Sectionwrapper = (Component, idName, backgroundProp, container1) =>
     function HOC(props) {
     // let  background = `bg-${backgroundProp}`
     // console.log("background:", background)
       return (
         <section
-         
-          className={`${styles.container} relative dark:bg-dark-containerBg ${backgroundProp}`}
+          className={`relative ${!container1? `${styles.container}`:`${styles.footer_container} border-primary_300 border-t-2`} relative dark:bg-dark-containerBg ${backgroundProp}`}
         >
-          <span className='hash-span' id={idName}>
+          <span className='hash-span absolute -top-16' id={idName}>
             &nbsp;
           </span>
   
