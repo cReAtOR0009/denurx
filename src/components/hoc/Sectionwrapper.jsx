88c426d31397670e6f7b30c23cffdcddfed0e4b9
cReAtOR0009 <epidnugoairdrop@gmail.com>
2024-10-18 02:46:30 +0100

    added a max width and conditional for rendering background for dark color

--- src/components/hoc/Sectionwrapper.jsx
+++ src/components/hoc/Sectionwrapper.jsx
@@ -7,7 +7,7 @@ const Sectionwrapper = (Component, idName, backgroundProp, container1) =>
     // console.log("background:", background)
       return (
         <section
-          className={`relative ${!container1? `${styles.container}`:`${styles.footer_container} border-primary_300 border-t-2`} relative dark:bg-dark-containerBg ${backgroundProp}`}
+          className={`relative ${!container1? `${styles.container}`:`${styles.footer_container} border-primary_300 border-t-2`} relative ${!container1? 'dark:bg-dark-containerBg':'dark:bg-dark-backgroundNav'}  {backgroundProp} max-w-[2500px]`}
         >
           <span className='hash-span absolute -top-16' id={idName}>
             &nbsp;
